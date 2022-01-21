56307ea16ecf2e8a49edf897de812df102f43eed
paul.albisetti <paul.albisetti@soxes.ch>
2022-01-21 16:37:16 +0100

    make django rest api and basic search

--- Backend/plantify/finder/views.py
+++ Backend/plantify/finder/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
     return render(request, 'finder/home.html')
+
+
+def search(request):
+    return {'some': 'shit'}
