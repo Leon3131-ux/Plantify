87aee04a78824365aef4cf117584b3942ae65199
paul.albisetti <paul.albisetti@soxes.ch>
2022-01-28 16:25:03 +0100

    start search

--- Backend/plantify/api/search.py
+++ Backend/plantify/api/search.py
@@ -1,20 +1,70 @@
+import json
+
+
 class Search:
 
 	def __init__(self, request):
 		self.request = request
-
-		self.response = {'default': 'shit'}
+		self.response = {
+			'error': ''
+		}
 
 		self.search_plant()
 
 	def search_plant(self):
-		self.get_pos()
+		"""
+		{
+		shadow: (number between 1 and 5),
+		height: (hight in meters)
+		season_to_bloom: (winter, summer, autum, spring)
+		}
+		:return:
+		"""
+		search_json = json.loads(search_plant)
 
-	def get_pos(self):
-		self.response['request'] = str(self.request)
+		if not search_json.get('shadow'):
+			self.response['error'] = 'parameter shadow is missing or empty'
+			return
+		if not search_json.get('height'):
+			self.response['error'] = 'parameter height is missing or empty'
+			return
+		if not search_json.get('season_to_bloom'):
+			self.response['error'] = 'parameter season_to_bloom is missing or empty'
 
-	def search_temp(self):
-		pass
+		# height used to estimate temperature and humidity
 
-	def get_soil(self):
-		pass
+		height = 500  # height in meters
+		humidity = 3  # still unknown
+
+		matched_plant = self.match_plant(search_json.get('shadow'),
+										 height,
+										 humidity,
+										 search_json.get('season_to_bloom'))
+
+		self.response = {
+			"name": matched_plant.get('name'),
+			"soil": matched_plant.get('soil')
+		}
+
+	def match_plant(self, shadow, height, humidity, season_to_bloom):
+		"""
+		returns dict in this format:
+		{
+		"name": name of the plant
+		"soil": sourness of soil
+		}
+
+		:param shadow:
+		:param height:
+		:param humidity:
+		:param season_to_bloom:
+		:return:
+		"""
+		print('mathing stuff to plant')
+
+		data = {
+			"name": "tomato",
+			"soil": "some stuff"
+		}
+
+		return data
--- Backend/plantify/api/views.py
+++ Backend/plantify/api/views.py
@@ -10,5 +10,16 @@ def apiOverview(request):
 
 
 def search(request):
+	"""
+	request must be a JSON containing the following:
+	{
+	shadow: (number between 1 and 5),
+	hight: (hight in meters)
+	season_to_bloom: (winter, summer, autum, spring)
+	}
+
+	:param request:
+	:return:
+	"""
 	search_obj = Search(request)
 	return JsonResponse(search_obj.response)
